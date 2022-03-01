3e0024d9512c1fa6323a159141b7ee727fcb486e
unkownshore <ashayed560@gmail.com>
2022-03-01 16:51:49 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/unkownshore"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me Out!
         </a>
       </header>
     </div>
